b74c37ad4d4552d63cb7f3b56288d471c8c6da1c
albertecos <144015396+albertecos@users.noreply.github.com>
2025-09-23 15:01:45 +0200

    skeleton setup done

---
+++ CarRental/frontend/screens/bookingDetails.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+const BookingDetail: React.FC = () => {
+    const navigation = useNavigation();
+
+    return (
+        <View>
+
+        </View>
+    )
+};
+
+export default BookingDetail;
---
+++ CarRental/frontend/screens/login.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+const Login: React.FC = () => {
+    const navigation = useNavigation();
+
+    return (
+        <View>
+
+        </View>
+    )
+};
+
+export default Login;
