3a9d9aa0ad5efef189f4560c9fdaf59ab22fcbe6
albertecos <144015396+albertecos@users.noreply.github.com>
2025-09-25 10:07:51 +0200

    start

---
+++ CarRental/frontend/components/cards/ConfirmationCard.tsx
@@ -0,0 +1,13 @@
+import {View} from "react-native";
+
+export default function ConfirmationCard(){
+    function confirm(){
+        console.log('Confirmed booking of car');
+    }
+
+    const imgsrc = require("../../../assets/car.png");
+
+    return (
+        <View></View>
+    )
+}
