ef91f9ef2f11c6dbfb31ffa0aa5b33383ff0908e
asta-strachotta <144019710+asta-strachotta@users.noreply.github.com>
2025-10-03 15:49:41 +0200

    Update App.tsx
    needed to add "NavigatorScreenParams" to import statement

--- CarRental/App.tsx
+++ CarRental/App.tsx
@@ -1,4 +1,4 @@
-import {NavigationContainer} from '@react-navigation/native';
+import {NavigationContainer, NavigatorScreenParams} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import Home from "./frontend/screens/home";
